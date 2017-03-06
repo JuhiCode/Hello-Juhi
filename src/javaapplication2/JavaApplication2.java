85cc07adac4fc59dc8ab1dd4db09621555107e70
JuhiCode <juhi.chand88@gmail.com>
2017-03-05 20:40:50 -0500

    first commit

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Juhi
+ */
+public class JavaApplication2 {
+
+    int count = 0;
+public int increment() {
+    return count += 1;
+}
+public int decrement() {
+   return count -= 1;}
+    
+}
